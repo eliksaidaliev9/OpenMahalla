9dae95d2aefa36d89a8b7d2b9f244dc8b7e2a0f4
Elyor <elik.saidaliev9@gmail.com>
2026-01-02 22:20:13 +0500

    Add Mahalla CRUD views

--- mahalla/views.py
+++ mahalla/views.py
@@ -1,3 +1,14 @@
+from rest_framework import viewsets, filters
+from users.permissions import IsStaffOrReadOnly
+from .models import Mahalla
+from .serializers import MahallaSerializer
 
 
+class MahallaViewSet(viewsets.ModelViewSet):
+    queryset = Mahalla.objects.all()
+    serializer_class = MahallaSerializer
+    permission_classes = [IsStaffOrReadOnly]
+
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name', 'district']
 
