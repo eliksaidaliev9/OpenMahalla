d1ef1d03035272fa781ca40c49d0f22a153965dc
Elyor <elik.saidaliev9@gmail.com>
2026-01-06 22:26:01 +0500

    Add users model and admin configuration

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'phone_number', 'role', 'is_active', 'is_staff', 'is_superuser')
+    list_filter = ('role', 'is_staff', 'is_superuser')
+    search_fields = ('username', 'phone_number')
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            if obj.role == 'applicant':
+                obj.is_staff = False
+                obj.is_superuser = False
+        super().save_model(request, obj, form, change)
--- users/models.py
+++ users/models.py
@@ -1,6 +1,42 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, PermissionsMixin, BaseUserManager
+from django.utils import timezone
 
 
-class User(AbstractUser):
+class UserManager(BaseUserManager):
+    def create_user(self, phone_number, password=None, username=None, **extra_fields):
+        if not phone_number:
+            raise ValueError("Phone number is required")
+        user = self.model(phone_number=phone_number, username=username, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
 
+    def create_superuser(self, phone_number, password=None, username=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self.create_user(phone_number, password, username, **extra_fields)
+
+
+class User(AbstractUser, PermissionsMixin):
+    ROLE_CHOICES = (
+        ('applicant', 'Applicant'),
+    )
+
+    phone_number = models.CharField(max_length=15, unique=True, null=True)
+    username = models.CharField(max_length=50, unique=True, null=True, blank=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.username if self.username else self.phone_number
