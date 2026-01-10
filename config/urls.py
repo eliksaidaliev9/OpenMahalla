022f27dc4325c5a0740cb4711018fe1332cc9f65
Elyor <elik.saidaliev9@gmail.com>
2026-01-10 22:48:04 +0500

    Add app URLs, JWT/Djoser auth, and API docs to config/urls.py

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,58 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from drf_yasg.generators import OpenAPISchemaGenerator
+from rest_framework import permissions
+from rest_framework_simplejwt.views import (
+    TokenVerifyView,
+    TokenRefreshView,
+    TokenObtainPairView
+)
+
+
+class JWTSchemaGenerator(OpenAPISchemaGenerator):
+    def get_security_definitions(self):
+        security_definitions = super().get_security_definitions()
+        security_definitions['Bearer'] = {
+            'type': 'apiKey',
+            'name': 'Authorization',
+            'in': 'header'
+        }
+        return security_definitions
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="OpenMahalla API",
+        default_version='v1',
+        description="OpenMahalla API documentation",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="elyor1996em@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+    generator_class=JWTSchemaGenerator,
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/users', include('users.urls')),
+
+    path('api/v1/users/', include('djoser.urls')),
+    path('api/v1/users/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/users/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/users/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
+    path('api/v1/appeals/', include('appeals.urls')),
+    path('api/v1/mahallas/', include('mahallas.urls')),
+
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
