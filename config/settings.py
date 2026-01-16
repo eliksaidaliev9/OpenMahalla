57dde46d2c9acb359f5d91706161ef75e5259307
Elyor <elik.saidaliev9@gmail.com>
2026-01-17 01:21:01 +0500

    Modify docker-compose configuration and update settings

--- config/settings.py
+++ config/settings.py
@@ -96,8 +96,8 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
