dc45670f03c57c46f3444800d36a392caed13f34
Elyor <elik.saidaliev9@gmail.com>
2026-01-19 23:25:49 +0500

    Update settings

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['openmahalla.uz', 'www.openmahalla.uz', '127.0.0.1', 'localhost']
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
