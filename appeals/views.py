4edc27e9168fdfcf73f4a4be689e1720004fa017
Elyor <elik.saidaliev9@gmail.com>
2026-01-22 16:48:04 +0500

    Update views in Appeals app

--- appeals/views.py
+++ appeals/views.py
@@ -5,7 +5,6 @@ from rest_framework.response import Response
 
 from .models import Appeal
 from .serializers import AppealSerializer, AppealAnswerSerializer
-from .services import mark_answered, mark_under_review
 from users.permissions import IsStaffOrReadOnly, IsOwnerOrReadOnly
 
 
@@ -23,21 +22,25 @@ class AppealViewSet(ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
-
-    @action(detail=True, methods=['post'], permission_classes=[IsStaffOrReadOnly])
-    def under_review(self, request, pk=None):
+    def retrieve(self, request, *args, **kwargs):
         appeal = self.get_object()
-        mark_under_review(appeal)
-        return Response({"detail": "Ko'rib chiqilyapti."})
+
+        if request.user.is_staff and appeal.status == Appeal.Status.NEW:
+            appeal.mark_under_review()
+
+        serializer = self.get_serializer(appeal)
+        return Response(serializer.data)
 
     @action(
         detail=True,
         methods=['post'],
         permission_classes=[IsStaffOrReadOnly],
-        serializer_class=AppealAnswerSerializer)
+        serializer_class=AppealAnswerSerializer
+    )
     def answer(self, request, pk=None):
         appeal = self.get_object()
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        mark_answered(appeal, serializer.validated_data['answer'])
+        appeal.mark_answered(serializer.validated_data['answer'])
+
         return Response({"detail": "Javob berildi."})
