3fb7feba5ad85fdf59a306fdd4df483a01dc9eb2
Elyor <elik.saidaliev9@gmail.com>
2026-01-22 15:36:44 +0500

    Update serializers in Users app

--- users/serializers.py
+++ users/serializers.py
@@ -20,3 +20,4 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'phone_number', 'username', 'role', 'date_joined']
+        ref_name = "OpenMahallaUser"
