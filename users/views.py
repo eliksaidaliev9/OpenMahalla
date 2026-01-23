7c4c0dd01b86ae8b23b71adaafa621fd92a4b96a
Elyor <elik.saidaliev9@gmail.com>
2026-01-23 19:40:45 +0500

    Update views

--- users/views.py
+++ users/views.py
@@ -1,26 +1,23 @@
 from rest_framework.decorators import action
-from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import viewsets, status
 
 from .models import User
 from .serializers import UserSerializer
 from .permissions import IsAdmin
 
 
-class UserViewSet(ModelViewSet):
+class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [IsAdmin]
 
-    @action(
-        detail=False,
-        methods=['post'],
-        permission_classes=[AllowAny],
-        url_path='create'
-    )
-    def create_user(self, request):
+    def get_permissions(self):
+        if self.action == 'create':
+            return [AllowAny()]
+        return [IsAdmin()]
+
+    def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
