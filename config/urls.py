e19694eb735122bcd1601ea06737859af04e7938
Elyor <elik.saidaliev9@gmail.com>
2026-01-23 16:55:14 +0500

    Update urls in Congif

--- config/urls.py
+++ config/urls.py
@@ -55,7 +55,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/users', include('users.urls')),
+    path('api/v1/', include('users.urls')),
 
     path('api/v1/users_auth/', include('djoser.urls')),
     path('api/v1/users_auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
