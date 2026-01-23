05f1fd16755c01628689c597a03c6f076b4d3882
Elyor <elik.saidaliev9@gmail.com>
2026-01-23 17:52:20 +0500

    Update permissions in Users

--- users/permissions.py
+++ users/permissions.py
@@ -1,43 +1,16 @@
 from rest_framework import permissions
 
 
-class IsOwnerOrReadOnly(permissions.BasePermission):
+class IsOwnerAndEditable(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+       return request.user.is_authenticated and obj.user == request.user and obj.status == 'new'
+
+
+class IsStaffOrAdmin(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-
-        user = request.user
-        if user.is_authenticated and user.is_superuser:
-            return True
-
-        if hasattr(obj, 'status') and hasattr(obj, 'user'):
-            if obj.user != request.user:
-                return False
-            return obj.status == 'new'
-
-        if obj == request.user:
-            if hasattr(request.user, 'appeals'):
-                if request.user.appeals.filter(status__in=['under_review', 'answered']).exists():
-                    return False
-            return True
-        return False
-
-
-class IsStaffOrReadOnly(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-        user = request.user
-        if user.is_authenticated and user.is_superuser:
-            return True
-
-        if user.is_authenticated and user.is_staff:
-            if hasattr(obj, 'status'):
-                return True
-            return False
-        return False
-
+        return request.user.is_authenticated and (request.user.is_staff and request.user.is_superuser)
 
 class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
