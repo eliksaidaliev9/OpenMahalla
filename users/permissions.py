6187dcdbe0523a433f490af98eaeac523239b88e
Elyor <elik.saidaliev9@gmail.com>
2026-01-24 20:09:16 +0500

    Update permissions

--- users/permissions.py
+++ users/permissions.py
@@ -1,9 +1,10 @@
 from rest_framework import permissions
+from appeals.models import Appeal
 
 
 class IsOwnerAndEditable(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-       return request.user.is_authenticated and obj.user == request.user and obj.status == 'new'
+       return request.user.is_authenticated and obj.user == request.user and obj.status == Appeal.Status.NEW
 
 
 class IsStaffOrAdmin(permissions.BasePermission):
@@ -14,7 +15,4 @@ class IsStaffOrAdmin(permissions.BasePermission):
 
 class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.is_superuser
-
-    def has_object_permission(self, request, view, obj):
         return request.user.is_authenticated and request.user.is_superuser
