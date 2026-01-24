ab8f0f7d3f76578d00f96e04860643654745cad2
Elyor <elik.saidaliev9@gmail.com>
2026-01-24 22:21:22 +0500

    Update settings

--- config/settings.py
+++ config/settings.py
@@ -134,7 +134,7 @@ AUTH_USER_MODEL = 'users.User'
 # REST Framework
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 5,
 
     'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.AllowAny',],
 
