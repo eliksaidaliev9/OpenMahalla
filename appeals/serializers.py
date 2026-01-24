37f10d2c825fda6590b0a268ba8c73fe441f8400
Elyor <elik.saidaliev9@gmail.com>
2026-01-24 23:28:50 +0500

    Update admin, models and serializers

--- appeals/serializers.py
+++ appeals/serializers.py
@@ -5,7 +5,7 @@ from .models import Appeal
 class AppealSerializer(serializers.ModelSerializer):
     class Meta:
         model = Appeal
-        fields = ['id', 'mahalla', 'category', 'description']
+        fields = ['id', 'mahalla', 'category', 'full_name', 'description']
         read_only_fields = ['user', 'status', 'answer', 'answered_at']
 
 class AppealAnswerSerializer(serializers.ModelSerializer):
