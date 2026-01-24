1c7ca4c92eacde7d7a8cec6c935065bad80c9ad0
Elyor <elik.saidaliev9@gmail.com>
2026-01-24 13:58:58 +0500

    Update settings

--- config/settings.py
+++ config/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql.psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('POSTGRES_DB'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
