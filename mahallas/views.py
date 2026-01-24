758e4222894e897aa6efaa135cd60f9b68331026
Elyor <elik.saidaliev9@gmail.com>
2026-01-24 22:43:42 +0500

    Update views

--- mahallas/views.py
+++ mahallas/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets, filters
+from rest_framework.permissions import IsAuthenticated
 from users.permissions import IsAdmin
 from .models import Mahalla
 from .serializers import MahallaSerializer
@@ -7,8 +8,10 @@ from .serializers import MahallaSerializer
 class MahallaViewSet(viewsets.ModelViewSet):
     queryset = Mahalla.objects.all()
     serializer_class = MahallaSerializer
-    permission_classes = [IsAdmin]
-
     filter_backends = [filters.SearchFilter]
     search_fields = ['name', 'district']
 
+    def get_permissions(self):
+        if self.action == 'list':
+            return [IsAuthenticated()]
+        return [IsAdmin()]
