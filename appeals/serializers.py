ee5e29e00b3718ce6abfb388d93f75c0b1ae7d26
Elyor <elik.saidaliev9@gmail.com>
2026-01-26 20:57:31 +0500

    Update serializers

--- appeals/serializers.py
+++ appeals/serializers.py
@@ -21,6 +21,7 @@ class AppealAnswerSerializer(serializers.ModelSerializer):
 class AppealListSerializer(serializers.ModelSerializer):
     mahalla = serializers.SlugRelatedField(slug_field='title', read_only=True)
     category = serializers.SlugRelatedField(slug_field='title', read_only=True)
+    user = serializers.SlugRelatedField(slug_field=)
 
     class Meta:
         model = Appeal
